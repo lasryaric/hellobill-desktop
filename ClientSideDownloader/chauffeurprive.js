18479e9773f6fd9168a0a376ec7cfc5ddd94d51f
Xavier Green <xav.green.95@gmail.com>
2016-06-10 19:34:16 -0700

    adding connector chauffeur-prive xav

---
+++ ClientSideDownloader/chauffeurprive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function down(data) {
+  const date = data.date;
+  return new Promise((yes, no) => {
+    $.get('https://app.chauffeur-prive.com/my_rides_ajax?iDisplayLength=9999')
+    .then((r) => {
+      var html = "";
+      r.aaData.forEach(function(entry) {
+        html += "<tr class='hellobill'><td>"+entry.join()+"</td></tr>";
+      });
+      document.querySelector('#rides tbody').innerHTML = html;
+      const elements = document.querySelectorAll('tr.hellobill');
+      const moment = window.__hellobill.utils.moment;
+      const regStr = moment(date, "YYYY-MM").format("MM/YY");
+      const okElements = _.filter(elements, function(elem) {
+        const dateText = elem.querySelector('td').textContent;
+        if (dateText === null) {
+          return false;
+        }
+        return dateText.match(regStr)
+      });
+      const links = window.__hb._.map(okElements, (order) => {
+        const link = order.querySelector('a[href*="facture"]:not([href*="format=full"])');
+        return link.href;
+      });
+      yes(links);
+    })
+  })
+}
+
+module.exports = {
+  down: down,
+}
