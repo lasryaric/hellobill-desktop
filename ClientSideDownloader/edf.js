6897275df97486b5ae2f5d779c4278db4dc15ab1
Xavier Green <xav.green.95@gmail.com>
2016-06-13 12:09:10 -0700

    Added fixed edf connector xav

--- ClientSideDownloader/edf.js
+++ ClientSideDownloader/edf.js
@@ -2,13 +2,14 @@
 
 function download(date, offset, done, next) {
   //date: 2016-01
+  //debugger;
   offset = offset || 0;
   const _ = window.__hellobill.utils._;
   const moment = window.__hellobill.utils.moment;
 
-  const regStr = moment(date, "YYYY-MM").locale('fr').format('L');
+  const regStr = moment(date, "YYYY-MM").format("MM/YYYY");
 
-  const allElements = document.querySelectorAll("'.factures tbody tr'");
+  const allElements = document.querySelectorAll('.factures tbody tr');
   const okElements = _.filter(allElements, function(elem) {
     const dateText = elem.querySelector('td a[href*="getFacturePDF"]').textContent;
     if (dateText === null) {
@@ -22,7 +23,7 @@ function download(date, offset, done, next) {
     return ;
   }
 
-  okElements[offset].click();
+  window.location = okElements[offset].querySelector('td a[href*="getFacturePDF"]').href;
 }
 
 module.exports = {
