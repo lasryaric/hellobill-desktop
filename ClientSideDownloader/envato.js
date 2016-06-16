4a0c1de55c74915b6d7de0c3c3a378046ae608a8
Xavier Green <xav.green.95@gmail.com>
2016-06-15 20:12:34 -0700

    Adding connector envato xav

---
+++ ClientSideDownloader/envato.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+function getInvoicesURLS(data) {
+ var date = data.date;
+
+ return new Promise((yes, no) => {
+   debugger;
+   const moment = window.__hellobill.utils.moment;
+   const dateRegStr = moment(date, "YYYY-MM").format("MMMM").substring(0,3).concat(moment(date, "YYYY-MM").format("[.*]YYYY"));
+   const orders = document.querySelectorAll('tr.statement__line');
+   const okOrders = window.__hb._.filter(orders, (order) => {
+     const dateDomElement = order.querySelector('td.statement__date');
+     if (!dateDomElement || !dateDomElement.textContent) {
+       return false;
+     }
+     console.log("@@@@",dateRegStr,"@@@@@",dateDomElement.textContent)
+     return !!dateDomElement.textContent.match(dateRegStr);
+   })
+
+   const links = window.__hb._.map(okOrders, (order) => {
+     const link = order.querySelector('a[href*="financial_document"]');
+     return link.href;
+   });
+   yes(links);
+ })
+}
+
+module.exports = {
+ getInvoicesURLS: getInvoicesURLS
+}
